feaafc68adcb6d6690733ac41c7b6a0072a73ce2
anujshashimal1bt <anuj.jayasekara@1billiontech.com>
2023-06-06 15:34:05 +0530

    updated

--- ValidationRules/src/Features/index.tsx
+++ ValidationRules/src/Features/index.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import SectionComponent from '../Components/sctionComponent/sectionComponent';
-
+import DisplayText from '../Components/displayText/displayText';
 
 import { Button } from 'antd';
 
 const ParentComponent: React.FC = () => {
     const [sections, setSections] = useState<{ key: string | number }[]>([{ key: 1 }]);
+    const [sectionOutput, setSectionOutput]  = useState<any[]>([]);
 
     let handleAddSection = () => {
         const newKey = Math.round(Math.random() * (10000));
@@ -13,23 +14,26 @@ const ParentComponent: React.FC = () => {
     }
 
     const handleRemoveSection = (sectionKey: any) => {
-        if(sections.length >= 2)
+        if (sections.length >= 2)
             setSections(sections.filter((section) => section.key !== sectionKey))
     }
 
     return (
         <div>
-            {sections.map((section) => (
-                <div> 
+            <div className='displayText'> <DisplayText fieldOutputData={[]} /> </div>
+                {sections.map((section) => (
                     <div>
-                        <SectionComponent key={section.key} sectionKey={section.key} />
+                        <div>
+                            <SectionComponent
+                                key={section.key}
+                                sectionKey={section.key}
+                                setSectionOutput={setSectionOutput} />
+                        </div>
+                        <div>
+                            <Button onClick={() => handleRemoveSection(section.key)}>Remove Clause</Button>
+                        </div>
                     </div>
-                    <div>
-                        <Button onClick={() => handleRemoveSection(section.key)}>Remove Clause</Button>
-                    </div>
-                 </div>
-                 
-      ))}
+                ))}
             <Button onClick={handleAddSection}>Add New Clause</Button>
         </div>
     )
